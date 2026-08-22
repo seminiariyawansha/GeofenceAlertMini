cb1b3560808e63406c9551cb85beb95dacae3036
seminiariyawansha <ariyawa-im23029@stu.kln.ac.lk>
2026-08-22 14:17:09 +0530

    Fix manifest structure: move receiver outside activity, add installLocation
    Receiver tag was nested inside activity causing AAPT build error. Also added preferExternal install location.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "com.example.geofencealertmini"
     compileSdk {
-        version = release(36) {
+        version = release(37) {
             minorApiLevel = 1
         }
     }
